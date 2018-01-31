ad23ad308ef6d1772d37988a3678fe0575660ead
Victoria Oh <veoh1989@gmail.com>
2018-01-31 15:51:17 -0800

    routes working

--- lab-liza/route/route-lotr.js
+++ lab-liza/route/route-lotr.js
@@ -3,6 +3,7 @@
 const Lotr = require('../model/lotr');
 const bodyParser = require('body-parser').json();
 const errorHandler = require('../lib/error-handler');
+const debug = require('debug')('http:route-lotr');
 
 module.exports = function(router) {
   router.route('/lotr/:_id?')
@@ -12,9 +13,15 @@ module.exports = function(router) {
           .then(lotr => res.status(200).json(lotr))
           .catch(err => errorHandler(err, res));
       }
-
-    // otherwise handle the case of no ID
-
+      return Lotr.find()
+        .then(lotr => lotr.map(lotr => lotr._id))
+        .then(lotr => res.status(200).json(lotr))
+        .catch(err => errorHandler(err, res));
+    })
+    .get(bodyParser, (req, res) => {
+      return Lotr.find()
+        .then(lotr => res.json(lotr.map(lotr => lotr._id)))
+        .catch(err => errorHandler(err, res));
     })
     .post(bodyParser, (req, res) => {
       new Lotr(req.body).save()
@@ -22,9 +29,13 @@ module.exports = function(router) {
         .catch(err => errorHandler(err, res));
     })
     .put(bodyParser, (req, res) => {
-
+      return Lotr.findByIdAndUpdate(req.params._id, req.body)
+        .then(() => res.sendStatus(204))
+        .catch(err => errorHandler(err, res));
     })
     .delete((req, res) => {
-
+      return Lotr.findByIdAndRemove(req.params._id)
+        .then(() => res.sendStatus(204))
+        .catch(err => errorHandler(err, res));
     });
 };
