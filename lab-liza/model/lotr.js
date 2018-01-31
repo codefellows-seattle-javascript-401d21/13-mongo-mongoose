aa94cd32bdd3bba14a5705c3ff94161d1806e647
Victoria Oh <veoh1989@gmail.com>
2018-01-31 15:09:41 -0800

    get one and post working

--- lab-liza/model/lotr.js
+++ lab-liza/model/lotr.js
@@ -5,10 +5,6 @@ const mongoose = require('mongoose');
 const LOTR = mongoose.Schema({
   'name': { type: String },
   'species': { type: String },
-  'aliases' : { type: String },
-  'age' : { type: Boolean },
-  'evil' : { type: Boolean },
-  'living': { type: Boolean },
 }, {timestamps: true});
 
 module.exports = mongoose.model('lotr', LOTR);
--- lab-liza/route/route-lotr.js
+++ lab-liza/route/route-lotr.js
@@ -1,20 +1,14 @@
 'use strict';
 
-const LOTR = require('../model/lotr');
+const Lotr = require('../model/lotr');
 const bodyParser = require('body-parser').json();
 const errorHandler = require('../lib/error-handler');
 
 module.exports = function(router) {
-  // router.get()
-  // router.post()
-
-  // Below is another example of mounting route methods to the router
   router.route('/lotr/:_id?')
     .get((req, res) => {
-    // debug(`${req.method}: ${req.url}`)
-
       if(req.params._id) {
-        return LOTR.findById(req.params._id)
+        return Lotr.findById(req.params._id)
           .then(lotr => res.status(200).json(lotr))
           .catch(err => errorHandler(err, res));
       }
@@ -23,7 +17,7 @@ module.exports = function(router) {
 
     })
     .post(bodyParser, (req, res) => {
-      new LOTR(req.body).save()
+      new Lotr(req.body).save()
         .then(lotr => res.status(201).json(lotr))
         .catch(err => errorHandler(err, res));
     })
