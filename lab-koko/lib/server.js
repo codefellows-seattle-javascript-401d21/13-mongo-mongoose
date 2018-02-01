693a5b9169613b10868b6687e9b975ecc21508c4
Koko Kassa <kowserk7@gmail.com>
2018-01-31 16:54:50 -0800

    POST GET PUT and DELETE all work

--- lab-koko/lib/server.js
+++ lab-koko/lib/server.js
@@ -11,17 +11,18 @@ const router = express.Router();
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
-app.use('api/v1/book', router);
-require('../route/route-books');
+app.use('/api/v1', router);
+require('../route/route-book')(router);
 app.use('/{0,}', (req, res) => errorHandler(new Error('Path error. Route not found.'), res));
 
 const server = module.exports = {};
 server.start = () => {
   return new Promise((resolve, reject) => {
-    if (server.isON) return reject(new Error('Server running. Cannot start server again,'));
+    if(server.isOn) return reject(new Error('Server running. Cannot start server again'));
+
     server.http = app.listen(PORT, () => {
       console.log(`Listening on ${PORT}`);
-      server.isON = true;
+      server.isOn = true;
       server.db = mongoose.connect(MONGODB_URI);
       return resolve(server);
     });
@@ -29,12 +30,13 @@ server.start = () => {
 };
 
 server.stop = () => {
-  return new Promise ((resolve, reject) => {
-    if(!server.isON) return reject(new Error('Server not running. Cannot sut server down'));
+  return new Promise((resolve, reject) => {
+    if(!server.isOn) return reject(new Error('Server not running. Cannot shut server down'));
+
     server.http.close(() => {
       console.log('Shutting down server');
       server.db.disconnect();
-      server.isON = false;
+      server.isOn = false;
       return resolve(server);
     });
   });
--- lab-koko/model/book.js
+++ lab-koko/model/book.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 
 const Book = mongoose.Schema({
   'summary': { type: String },
+  'genre': {type: String},
   'author' : { type: String, require: true },
   'title': { type: String, require: true },
 }, {timestamps: true});
 
-module.exports = mongoose.model('books', Book);
+module.exports = mongoose.model('book', Book);
