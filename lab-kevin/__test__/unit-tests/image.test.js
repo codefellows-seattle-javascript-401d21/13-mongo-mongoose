83a14bb2f81c5e73d2b2824bfdd06590b619717a
Kevin Miller <kevin@driftabout.com>
2018-01-31 23:24:25 -0800

    fixed linter error

--- lab-kevin/__test__/unit-tests/image.test.js
+++ lab-kevin/__test__/unit-tests/image.test.js
@@ -4,19 +4,19 @@ const Image = require('../../model/image');
 
 describe('NOTE unit testing', function() {
   // this.mockItem = {subject: 'this', comment: 'that'};
-  // new Note(this.mockItem.subject, this.mockItem.comment)
-  //   .then(item => this.note = item);
-  // it('should be an object', () => {
-  //   console.log('note',this.note);
-  //   expect (this.note).toBeInstanceOf(Object);
-  // });
+  new Image({file_path: '/',file_name: 'h' })
+    .then(item => this.image = item);
+  it('should be an object', () => {
+    console.log('image',this.image);
+    expect (this.Image).toBeInstanceOf(Object);
+  });
   // it('should have a uuid', () => {
-  //   expect (this.note.id).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+  //   expect (this.image.id).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
   // });
   // it('should have a subject', () => {
-  //   expect (this.note.subject).not.toBeNull();
+  //   expect (this.image.subject).not.toBeNull();
   // });
   // it('should have a comment', () => {
-  //   expect (this.note.comment).not.toBeNull();
+  //   expect (this.image.comment).not.toBeNull();
   // });
 });
